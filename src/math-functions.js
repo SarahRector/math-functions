20be75d85798d2c25d4b23d18c3a926a83ccf6c1
SarahRector <rector.sarah.r@gmail.com>
2020-07-10 11:51:27 -0700

    added multipy function

--- src/math-functions.js
+++ src/math-functions.js
@@ -30,7 +30,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
-
+    const multTotal = (Number(a * b));
+    const multString = `The product of ${a} and ${b} is ${multTotal}.`;
+    const myArray = [];
+    myArray.push(multTotal, multString);
+    return myArray;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
