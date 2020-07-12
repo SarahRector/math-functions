0b2ff20920ab32e641125da76dad9596d442f084
SarahRector <rector.sarah.r@gmail.com>
2020-07-12 15:49:56 -0700

    added sum and multipy function

--- src/math-functions.js
+++ src/math-functions.js
@@ -140,7 +140,12 @@ This function should be dynamic, accepting an array of any length.
 */
 
 export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    let dynamicProduct = 1;
+    for(let i = 0; i < dynamicArray.length; i++) {
+        dynamicProduct = multiply(dynamicArray[i], dynamicProduct)[0];
+    }
+    const productString = `The numbers ${dynamicArray} have a product of ${dynamicProduct}.`;
+    return [dynamicProduct, productString];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
